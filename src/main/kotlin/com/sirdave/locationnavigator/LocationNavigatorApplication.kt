d9610651977ce98da20098c1e34ce86b2be7ce45
Abiola David <davidolanrewaju2000@gmail.com>
2024-02-27 05:44:58 +0100

    Initialized project

---
+++ src/main/kotlin/com/sirdave/locationnavigator/LocationNavigatorApplication.kt
@@ -0,0 +1,11 @@
+package com.sirdave.locationnavigator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LocationNavigatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<LocationNavigatorApplication>(*args)
+}
