37f29b0eef3d9feebf40c6065fc896f4a9a3a4bd
Abiola David <davidolanrewaju2000@gmail.com>
2024-02-27 08:11:52 +0100

    Addded controller

---
+++ src/main/kotlin/com/sirdave/locationnavigator/place/PlaceController.kt
@@ -0,0 +1,68 @@
+package com.sirdave.locationnavigator.place
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+@RequestMapping(path = ["/places"])
+class PlaceController(private val service: PlaceService) {
+
+    @PostMapping
+    fun createNewPlace(
+        @RequestParam name: String,
+        @RequestParam alias: String,
+        @RequestParam longitude: Double,
+        @RequestParam latitude: Double,
+        @RequestPart images: List<MultipartFile>,
+        @RequestParam placeType: String,
+        @RequestParam(required = false) category: String?
+    ): ResponseEntity<Place> {
+        val place = service.createNewPlace(
+            name = name,
+            alias = alias,
+            longitude = longitude,
+            latitude = latitude,
+            images = images,
+            placeType = placeType,
+            category = category
+        )
+        return ResponseEntity(place, HttpStatus.CREATED)
+    }
+
+    @PutMapping("/{id}")
+    fun updatePlace(
+        @PathVariable("id") id: Long,
+        @RequestParam(required = false) name: String?,
+        @RequestParam(required = false) alias: String?,
+        @RequestParam(required = false) longitude: Double?,
+        @RequestParam(required = false) latitude: Double?,
+        @RequestParam(required = false) placeType: String?,
+        @RequestParam(required = false) category: String?
+    ): ResponseEntity<Place> {
+        val place = service.updatePlace(
+            id = id,
+            name = name,
+            alias = alias,
+            longitude = longitude,
+            latitude = latitude,
+            type = placeType,
+            category = category
+        )
+        return ResponseEntity(place, HttpStatus.OK)
+    }
+
+    @GetMapping
+    fun searchPlaces(
+        @RequestParam(required = false) name: String?,
+        @RequestParam(required = false) type: String?,
+    ): ResponseEntity<List<Place>> {
+        println("name is $name")
+        println("type is $type")
+
+        val places = if (type.isNullOrBlank()) name?.let { service.searchPlaces(name = it) }
+        else service.getPlacesByPlaceType(type)
+        return ResponseEntity(places, HttpStatus.OK)
+    }
+}
